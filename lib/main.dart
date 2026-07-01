f41228f1ee857a51aae9f44fbc78428982da6333
Jocelyn <angelesrjocelyn180@gmail.com>
2026-06-30 21:34:10 -0600

    CONEXION A BD

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:sapi/Login.dart';
 import 'package:sapi/Administrador/InicioAdmin.dart';
 import 'package:sapi/Padre/alumno/InicioAlumno.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
